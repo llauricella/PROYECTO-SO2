7f9fab528eeb35566c7e09eb93a540282d51e738
Luigi Lauricella <l.lauricella@correo.unimet.edu.ve>
2026-03-02 20:04:20 -0400

    Add: Inicializacion del proyecto

---
+++ src/main/java/com/mycompany/proyecto/so2/PROYECTOSO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.so2;
+
+/**
+ *
+ * @author Luigi Lauricella & Sebastian Gonzalez
+ */
+public class PROYECTOSO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
